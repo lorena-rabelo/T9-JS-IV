9053a1feeb7c59d239912f9fa6b95d681e9f8cdc
Lorena Rabelo <ola.lorenarabelo@gmail.com>
2020-06-05 09:24:47 -0300

    ...

--- revisao/exercicios_objeto/analise_objetos.js
+++ revisao/exercicios_objeto/analise_objetos.js
@@ -1,15 +1,34 @@
 //Dado o objeto, analise a forma que foi declarada. 
 //Em seguida, ajuste o objeto produtos da forma correta de se declarar.
+// let produtos = {
+//   videos: {
+//     comedia: ["comedia1""comedia2"];
+//     romance ["romance1", "romance2"]
+//   },
+//   revistas: [
+//     moda = ["lalala", "lililili"],
+//     saude = ["lalala", "lililili"],
+//   ]
+//   jogos {
+//     rpg ["rpg1", "rpg2", rpg3],
+//     acao: ["acao1", "God of War"]
+//   }
+
+
 let produtos = {
   videos: {
-    comedia: ["comedia1""comedia2"];
-    romance ["romance1", "romance2"]
+    comedia: ["comedia1","comedia2"],
+    romance: ["romance1", "romance2"]
+    },
+  revistas: {
+    moda: ["lalala", "lililili"],
+    saude: ["lalala", "lililili"]
   },
-  revistas: [
-    moda = ["lalala", "lililili"],
-    saude = ["lalala", "lililili"],
-  ]
-  jogos {
-    rpg ["rpg1", "rpg2", rpg3],
+    jogos: {
+    rpg:  ["rpg1", "rpg2", "rpg3"],
     acao: ["acao1", "God of War"]
   }
+}
+
+console.log(produtos)
+
---
+++ revisao/exercicios_objeto/teste.js
@@ -0,0 +1,12 @@
+class Signo {
+  constructor(nome,elemento, regente) {
+    this.nome = nome;
+    this.elemento = elemento;
+    this.regente = regente;
+  }
+}
+
+const lorena = new Signo('touro', 'terra', 'plutão');
+
+
+console.log (lorena)
